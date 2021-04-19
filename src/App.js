45c82dfaa0aafe81c6b30090bf984f1565364a2c
Sergey <GreyTob@inbox.ru>
2021-04-19 20:59:23 +0500

    create Layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react"
+import Layout from "./hoc/Layout/Layout"
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello React</h1>
+      <Layout>
 
-      </div>
+
+        <div style={{width: 400, border: '1px solid black'}}>
+          <h1>Layout works</h1>
+        </div>
+      </Layout>
     )
   }
 }
