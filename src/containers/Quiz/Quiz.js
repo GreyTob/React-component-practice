0bcc51a00a8be1772abec2547d2244fd164b7737
Sergey <GreyTob@inbox.ru>
2021-04-23 16:32:00 +0500

    checks answer

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -33,6 +33,14 @@ class Quiz extends Component {
   }
 
   onAnswerClickHandler = (answerId) => {
+
+    if (this.state.answerState) {
+      const key = Object.keys(this.state.answerState)[0]
+      if (this.state.answerState[key] === 'success') {
+        return
+      }
+    }
+
     console.log('answerId: ', answerId)
 
     const question = this.state.quiz[this.state.activeQuestions]
