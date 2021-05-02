01b921283e06d7385917ba78bf3dc70a7e13a61a
Sergey <GreyTob@inbox.ru>
2021-05-02 17:24:46 +0500

    validation form part 1

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,12 +4,64 @@ import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
+  state = {
+    formControls: {
+      email: {
+        type: 'email',
+        value: '',
+        label: 'Email',
+        errorMessage: 'Введите корректный Email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        type: 'password',
+        value: '',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный Пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+      },
+    },
+  }
+
   loginHandler = () => {}
 
   registerHandler = () => {}
 
   submitHandler = (event) => {
     event.preventDefault()
+  }
+
+  onChanheHandler = (Event, controlName) => {
+    console.log(`${controlName}:`, Event.target.value)
+  }
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          errorMessage={control.errorMessage}
+          shouldValidate={!!control.validation} //привожу к булевому типу - !!
+          onChange={(Event) => this.onChanheHandler(Event, controlName)}
+        />
+      )
+    })
   }
 
   render() {
@@ -23,21 +75,7 @@ export default class Auth extends Component {
             onSubmit={this.submitHandler}
             className={classes.AuthForm}
           >
-            <Input
-              type="text"
-              value="Логин"
-              label="Email"
-              onChange=""
-              errorMessage={'test'}
-            />
-
-            <Input
-              type="password"
-              value="password"
-              label="Пароль"
-              onChange=""
-              errorMessage={'test2'}
-            />
+            {this.renderInputs()}
 
             <Button type="success" onClick={this.loginHandler}>
               Войти
