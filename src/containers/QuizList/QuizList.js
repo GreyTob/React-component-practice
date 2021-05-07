23d14b403a1d349c4f10de4b410ad5439d7dc0b8
Sergey <GreyTob@inbox.ru>
2021-05-07 14:13:45 +0500

    add axios library

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import classes from './QuizList.module.css'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
 
 export default class QuizList extends Component {
   renderQuizes() {
@@ -12,6 +13,17 @@ export default class QuizList extends Component {
       )
     })
   }
+
+  // componentDidMount() {
+  //   //get-запрос для проверки базы данных axios
+  //   axios
+  //     .get(
+  //       'https://reacr-quiz-33e60-default-rtdb.europe-west1.firebasedatabase.app/quiz.json'
+  //     )
+  //     .then((response) => {
+  //       console.log(response)
+  //     })
+  // }
 
   render() {
     return (
